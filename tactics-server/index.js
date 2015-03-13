589e243954d3a24d84445acd7ab6bd14b97792a3
Gregori Kanatzidis <kanatzidisg@gmail.com>
2015-03-12 22:45:14 -0500

    cleanup

--- tactics-server/index.js
+++ tactics-server/index.js
@@ -14,27 +14,25 @@ io.on('connection', function (socket) {
     console.log('new game');
     io.emit('games', Games);
     socket.emit('gameNo', Games.length-1);
-    socket.emit('playerNo', 0);
   });
   socket.on('joinGame', function(gameNo) {
     Games[gameNo].push(me);
     opponent = Connections[Games[gameNo][0]];
     opponent.emit('opponent', me);
-    //socket.emit('playerNo', 1);
   });
-  socket.on('opponent', function(player) {
+  socket.on('opponent', function(playerId) {
     opponent = Connections[playerId];
   });
   console.log('connected');
-  socket.on('resetTurn', function (playerNo, gameNo) {
+  socket.on('resetTurn', function () {
     console.log('resetTurn');
     opponent.emit('resetTurn');
   });
-  socket.on('endTurn', function (playerNo, gameNo) {
+  socket.on('endTurn', function () {
     console.log('endTurn');
     opponent.emit('endTurn');
   });
-  socket.on('mark', function (stackNo, playerNo, gameNo) {
+  socket.on('mark', function (stackNo) {
     console.log(stackNo);
     opponent.emit('mark', stackNo);
   });
